b9485b260855da0460e62098f0fbbe9f195ecb0f
Eric Torgersen <etorgersen@albany.edu>
2018-12-16 19:36:00 -0500

    Add back create_radar_commands

--- selfdrive/car/honda/hondacan.py
+++ selfdrive/car/honda/hondacan.py
@@ -1,6 +1,8 @@
 import struct
+
+import common.numpy_fast as np
 from selfdrive.config import Conversions as CV
-from selfdrive.car.honda.values import CAR, HONDA_BOSCH
+from selfdrive.car.honda.values import CAR, HONDA_BOSCH, VEHICLE_STATE_MSG
 
 # *** Honda specific ***
 def can_cksum(mm):
@@ -123,6 +125,25 @@ def create_ui_commands(packer, pcm_speed, hud, car_fingerprint, idx):
     commands.append(packer.make_can_msg('RADAR_HUD', 0, radar_hud_values, idx))
   return commands
 
+def create_radar_commands(v_ego, car_fingerprint, new_radar_config, idx):
+  """Creates an iterable of CAN messages for the radar system."""
+  commands = []
+  v_ego_kph = np.clip(int(round(v_ego * CV.MS_TO_KPH)), 0, 255)
+  speed = struct.pack('!B', v_ego_kph)
+
+  msg_0x300 = ("\xf9" + speed + "\x8a\xd0" +
+               ("\x20" if idx == 0 or idx == 3 else "\x00") +
+               "\x00\x00")
+  msg_0x301 = VEHICLE_STATE_MSG[car_fingerprint]
+
+  idx_0x300 = idx
+  if car_fingerprint == CAR.CIVIC:
+    idx_offset = 0xc if new_radar_config else 0x8   # radar in civic 2018 requires 0xc
+    idx_0x300 += idx_offset
+
+  commands.append(make_can_msg(0x300, msg_0x300, idx_0x300, 1))
+  commands.append(make_can_msg(0x301, msg_0x301, idx, 1))
+  return commands
 
 def spam_buttons_command(packer, button_val, idx):
   values = {
