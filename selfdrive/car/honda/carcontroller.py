c011b9a9221adbb3a2b009d2e879ae2f095d334c
Neil Aronson <neilaronson@gmail.com>
2019-03-08 05:56:05 +0000

    change maxbrake value to 382 (EXPERIMENTAL)

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -147,7 +147,7 @@ class CarController(object):
     # **** process the car messages ****
 
     # *** compute control surfaces ***
-    BRAKE_MAX = 1024/4
+    BRAKE_MAX = 382
     if CS.CP.carFingerprint in (CAR.ACURA_ILX):
       STEER_MAX = 0xF00
     elif CS.CP.carFingerprint in (CAR.CRV, CAR.ACURA_RDX):
